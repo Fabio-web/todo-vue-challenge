1e94e67dcc7be37a7ef71320e7366474cfe1c895
Fabio-web <fabio.maccotta@ynov.com>
2022-02-13 20:16:54 +0100

    ✒️ upgrade homePage (tab, form, tasks)

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
    <div class="homePage">
+      <Tab :tabs="['All', 'Active', 'Completed']" justify="center" v-model="current"/>
+      <div class="form">
+         <Input placeholder="add details" full-width label="" v-model="inputDetail"/>
+         <Button color="primary" end-icon="add">Add</Button>
+      </div>
+      <div class="taskList">
+         <Checkbox v-model="checked[key]" strikethrough v-for="(_, key) in checked" :key="key">
+            <span class="taskText">Do coding challenges</span>
+         </Checkbox>
+      </div>
    </div>
 </template>
 
@@ -8,7 +18,19 @@
 </style>
 
 <script>
+import Input from "../components/input"
+import Button from "../components/button"
+import Tab from "../components/tab"
+import Checkbox from "../components/checkbox"
 
 export default {
+   components: { Checkbox, Tab, Button, Input },
+   data() {
+      return {
+         current: 0,
+         checked: [true, false, true, false, false],
+         inputDetail: ""
+      }
+   }
 }
 </script>
