0af585d34344091e9b8fe9f5e12ad14e05a8cdbc
Fabio-web <fabio.maccotta@ynov.com>
2022-02-13 19:39:23 +0100

    ➕ checkbox component

---
+++ components/checkbox/index.vue
@@ -0,0 +1,34 @@
+<template>
+   <label :class="['checkbox', (strikethrough && checked) && 'strikethrough']">
+      <input type="checkbox" :value="checked" ref="checkbox" @change="onChange">
+      <span class="checkbox-text"><slot/></span>
+   </label>
+</template>
+
+<style lang="scss">
+@import "style";
+</style>
+
+<script>
+import Vue from "vue"
+
+export default Vue.extend({
+   name: "Checkbox",
+   props: {
+      checked: Boolean,
+      strikethrough: Boolean,
+   },
+   model: {
+      prop: 'checked',
+      event: 'changeCheck'
+   },
+   mounted() {
+      this.$refs.checkbox.checked = this.checked
+   },
+   methods: {
+      onChange(event) {
+         this.$emit('changeCheck', event.target.checked);
+      }
+   }
+})
+</script>
