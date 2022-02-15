c4474e29606f3f79db7ae00d8c32a4b1246b055e
Fabio-web <fabio.maccotta@ynov.com>
2022-02-15 02:46:35 +0100

    ✅ dynamic todo list stored in local ✒️ update checkbox component

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,20 @@
 <template>
    <div class="homePage">
       <Tab :tabs="['All', 'Active', 'Completed']" justify="center" v-model="current"/>
-      <div class="form">
+      <form class="form" @submit.prevent="postTask" v-if="current !== 2">
          <Input placeholder="add details" full-width label="" v-model="inputDetail"/>
-         <Button color="primary" end-icon="add">Add</Button>
+         <Button type="submit" color="primary" end-icon="add">Add</Button>
+      </form>
+      <div class="taskList" v-if="tasks.length > 0">
+         <div class="task" v-for="(task, key) in tasks" :key="key" v-if="showTasks(key)">
+            <Checkbox strikethrough v-model="tasks[key].completed" @change.native="updateTask">
+               {{ task.task }}
+            </Checkbox>
+            <span class="delete material-icons-outlined" v-if="current === 2" @click="deleteTask(key)">delete</span>
+         </div>
       </div>
-      <div class="taskList">
-         <Checkbox v-model="checked[key]" strikethrough v-for="(_, key) in checked" :key="key">
-            <span class="taskText">Do coding challenges</span>
-         </Checkbox>
+      <div class="delAll" v-if="current === 2">
+         <Button start-icon="delete" color="danger" size="sm" @click.native="deleteTask(-1)">delete all</Button>
       </div>
    </div>
 </template>
@@ -28,8 +34,46 @@ export default {
    data() {
       return {
          current: 0,
-         checked: [true, false, true, false, false],
-         inputDetail: ""
+         inputDetail: "",
+         tasks: []
+      }
+   },
+   mounted() {
+      this.tasks = this.getTasks() ?? []
+   },
+   methods: {
+      postTask() {
+         if(this.tasks.find(v => v.task === this.inputDetail) || !this.inputDetail) return false
+         this.tasks.push({ task: this.inputDetail, completed: false })
+         this.inputDetail = ""
+
+         this.updateTask()
+      },
+      getTasks() {
+         return JSON.parse(localStorage.getItem("tasks"))
+      },
+      updateTask() {
+         localStorage.setItem("tasks", JSON.stringify(this.tasks))
+      },
+      deleteTask(index) {
+         if(index === -1) {
+            this.tasks = this.tasks.filter(task => !task.completed)
+         } else {
+            if(!this.tasks[index]) return false
+            this.tasks.splice(index, 1)
+         }
+
+         this.updateTask()
+      },
+      showTasks(index) {
+         switch (this.current) {
+            case 1:
+               return !this.tasks[index].completed
+            case 2:
+               return this.tasks[index].completed
+            default:
+               return true
+         }
       }
    }
 }
